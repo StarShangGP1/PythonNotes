3651c693fe54c263f3719964ea35e6616b1331f1
Yoke123 <2514407937@qq.com>
2018-04-25 10:03:59 +0800

    oop

--- oop.py
+++ oop.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from types import MethodType
+
 print('------模块------')
 
 print('------oop------')
@@ -84,3 +86,82 @@ print(dir(Cat()))
 
 del cat.age
 print(cat)
+
+print('------__slots__------')
+
+class Student(object):
+	__slots__ = ('name', 'age') # 用tuple定义允许绑定的属性名称
+
+s = Student()
+s.name = 'Tom'
+print(s.name)
+
+def set_age(self, age):
+		self.age = age
+
+# s.set_age = MethodType(set_age, s) # 给实例绑定一个方法
+# s.set_age(25)
+# print(s.age)
+
+# 给class绑定方法
+Student.set_age = MethodType(set_age, Student)
+
+
+class Students(object):
+
+	@property
+	def score(self):
+		return self.__score
+
+	@score.setter
+	def score(self, value):
+		if not isinstance(value, int):
+			raise ValueError('score must be an integer!')
+		if value < 0 or value > 100:
+			raise ValueError('score must between 0 ~ 100!')
+		self.__score = value
+s1 = Students()
+s1.score = 60 # OK，实际转化为s.set_score(60)
+print(s1.score) # OK，实际转化为s.get_score()
+
+print()
+print('-------多重继承-------')
+
+class CarnivorousMixIn(object):
+	pass
+
+class HerbivoresMixIn(object):
+	pass
+
+class RunnableMixIn(object):
+	def run(self):
+		print('running...')
+
+class FlyableMixIn(object):
+	def fly(self):
+		print('flying...')
+
+class Animal(object):
+	pass
+
+class Mammal(object):
+	pass
+
+class Bird(Animal):
+	pass
+
+class Dog(Mammal, RunnableMixIn, CarnivorousMixIn):
+	pass
+
+class Bat(Mammal, FlyableMixIn, HerbivoresMixIn):
+	pass
+
+class Parrot(Bird):
+	pass
+
+class Ostrich(Bird):
+	pass
+
+
+print()
+print('-------定制类-------')
