6d96da9d6102dc74fbf856a6ecb6a9c1df778635
Yoke123 <2514407937@qq.com>
2018-05-04 09:39:47 +0800

    module etc...

--- module.py
+++ module.py
@@ -15,6 +15,37 @@
  *           ░     ░ ░      ░  ░
  */            
 '''
+from datetime import datetime
+from collections import namedtuple, deque, defaultdict
 
 print('------内建模块------')
+
+print('------datetime------')
+
+now = datetime.now()
+print(now, type(now))
+
+dt = datetime(2017, 5, 3, 9, 45)
+print(dt.timestamp())
+
+dt2 = dt.timestamp()
+print(datetime.fromtimestamp(dt2))
+print(datetime.utcfromtimestamp(dt2))
+
+cday = datetime.strptime('2015-6-1 18:19:59', '%Y-%m-%d %H:%M:%S')
+print(cday)
+
+print(datetime.now().strftime('%a, %b %d %H:%M'))
+
+
+print('-------collections-------')
+
+Point = namedtuple('Point', ['x', 'y'])
+p = Point(1, 2)
+print(type(p))
+
+q = deque(['a', 'b', 'c'])
+q.append('x')
+q.appendleft('y')
+print(q)
 
